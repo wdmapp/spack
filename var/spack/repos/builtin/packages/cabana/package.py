15778b7e103836bbab4dfcec0d5683b5f4b48a93
Eric Suchyta <eric.d.suchyta@gmail.com>
2020-05-08 06:01:20 -0400

    Kokkos/Cabana that worked on Summit

--- var/spack/repos/builtin/packages/cabana/package.py
+++ var/spack/repos/builtin/packages/cabana/package.py
@@ -14,6 +14,7 @@ class Cabana(CMakePackage):
     url      = "https://github.com/ECP-copa/Cabana/archive/0.1.0.tar.gz"
 
     version('develop', branch='master')
+    version('suchyta', git='https://github.com/suchyta1/Cabana.git', branch='master')
     version('0.3.0', sha256='fb67ab9aaf254b103ae0eb5cc913ddae3bf3cd0cf6010e9686e577a2981ca84f')
     version('0.2.0', sha256='3e0c0e224e90f4997f6c7e2b92f00ffa18f8bcff72f789e0908cea0828afc2cb')
     version('0.1.0', sha256='3280712facf6932b9d1aff375b24c932abb9f60a8addb0c0a1950afd0cb9b9cf')
@@ -27,9 +28,13 @@ class Cabana(CMakePackage):
     depends_on("cmake@3.9:", type='build')
     depends_on("cuda", when="+cuda")
 
-    depends_on("kokkos-cmake +serial +aggressive_vectorization cxxstd=11")
-    depends_on("kokkos-cmake +openmp", when="+openmp")
-    depends_on("kokkos-cmake +cuda", when="+cuda")
+    kokkos = "kokkos"
+    if kokkos == "kokkos-cmake":
+        depends_on("{0} +serial +aggressive_vectorization cxxstd=11".format(kokkos))
+    else:
+        depends_on("{0} +serial +aggressive_vectorization cxxstd=c++11".format(kokkos))
+    depends_on("{0} +openmp".format(kokkos), when="+openmp")
+    depends_on("{0} +cuda".format(kokkos), when="+cuda")
 
 
     def cmake_args(self):
@@ -40,14 +45,19 @@ class Cabana(CMakePackage):
         if self.spec.satisfies('+mpi'):
             options += ["-DCabana_ENABLE_MPI=ON"]
 
+        if self.spec.satisfies('@suchyta') or self.spec.satisfies('@:0.2.99'):
+            if self.spec.satisfies('+cuda'):
+                options += ["-DCabana_ENABLE_Cuda=ON"]
+            if self.spec.satisfies('+openmp'):
+                options += ["-DCabana_ENABLE_OpenMP=ON"]
+
         if self.spec.satisfies("+cuda"):
             #env['NVCC_WRAPPER_DEFAULT_COMPILER'] = self.spec['mpi'].mpicxx
             if self.spec.satisfies('%gcc'):
                 env['NVCC_WRAPPER_DEFAULT_COMPILER'] = self.compiler.cxx
-                #env['CXX'] = self.compiler.cxx
             elif self.spec.satisfies('%pgi'):
                 env['NVCC_WRAPPER_DEFAULT_COMPILER'] = 'g++'
-            options += ['-DCMAKE_CXX_COMPILER={0}'.format(join_path(self.spec['kokkos-cmake'].prefix.bin, 'nvcc_wrapper'))]
+            options += ['-DCMAKE_CXX_COMPILER={0}'.format(join_path(self.spec[self.kokkos].prefix.bin, 'nvcc_wrapper'))]
             """
             if self.spec.satisfies("+mpi"):
                 options += ['-DMPI_CXX_COMPILER={0}'.format(self.spec['mpi'].mpicxx)]
